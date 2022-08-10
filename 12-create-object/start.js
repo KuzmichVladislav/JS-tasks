7d0600f50c9069461fe78f0401b9182e7a136db9
Kuzmich Vladislav <kyzmenoid@gmail.com>
2022-08-10 19:51:02 +0300

    11-30 task

---
+++ 12-create-object/start.js
@@ -0,0 +1,19 @@
+/** ЗАДАЧА 12 - Создание объекта
+ *
+ * 1. Создайте объект с тремя свойствами:
+ *  - name
+ *  - surname
+ *  - favoriteNumber
+ *
+ * 2. Выведите в консоль строку
+ * "My name is <name> <surname> and my favorite number is <favoriteNumber>"
+ */
+
+const user = {
+  name: 'Joe',
+  surname: 'Biden',
+  favoriteNumber: 11
+};
+
+console.log(
+    `My name is ${user.name} ${user.surname} and my favorite number is ${user.favoriteNumber}`);
